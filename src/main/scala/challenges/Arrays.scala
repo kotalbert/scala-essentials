6cf5d52d6698e6ed394e70238fb27f7da794156d
Pawel Daniluk <pawel.daniluk@kotalbert.net>
2022-12-03 19:23:22 +0100

    Implement property testing for Arrays functions

--- src/main/scala/challenges/Arrays.scala
+++ src/main/scala/challenges/Arrays.scala
@@ -4,7 +4,7 @@ object Arrays extends App {
 
 def sum(a: Array[Int]): Int = a.sum
 
-def avg(a: Array[Int]): Int = a.sum/a.length
+def avg(a: Array[Int]): Double = a.sum/a.length.toDouble
 def min(a: Array[Int]): Int = a.min
 
 def max(a: Array[Int]): Int = a.max
--- src/test/scala/chellenges/ArraysTests.scala
+++ src/test/scala/chellenges/ArraysTests.scala
@@ -1,35 +1,28 @@
 package chellenges
 
 import challenges.Arrays
-
-import scala.NotImplementedError
+import org.scalatest.prop.TableDrivenPropertyChecks._
+import org.scalatest.matchers.should.Matchers._
 import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.prop.TableFor5
 
 private class ArraysTests extends AnyFunSuite:
 
   // could be improved
   // https://stackoverflow.com/a/65378521
-  class Expected(val a: Array[Int], val sum: Int, val avg: Int, val min: Int, val max: Int)
-  val testParams: List[Expected] =List(
-    new Expected(Array(1, 2, 3), 6, 2, 1, 3)
+  // todo: implement as property based tests
+  //
+  val examples: TableFor5[Array[Int], Int, Double, Int, Int] = Table(
+    ("array", "sum", "avg", "min", "max"),
+    (Array(1, 2, 3), 6, 2d, 1, 3),
+    (Array(-2, -1, 0), -3, -1d, -2, 0),
   )
 
-  for (expected <- testParams) {
-    test("sum function") {
-      assert(Arrays.sum(expected.a) === expected.sum)
-    }
-
-    test("avg function") {
-      assert(Arrays.avg(expected.a) === expected.avg)
-    }
-
-    test("min function") {
-      assert(Arrays.min(expected.a) === expected.min)
-    }
-    test("max function") {
-      assert(Arrays.max(expected.a) === expected.max)
-    }
+  forAll(examples) { (array: Array[Int], sumExpected: Int, avgExpected: Double, minExpected: Int, maxExpected: Int) =>
+    Arrays.sum(array) should equal(sumExpected)
+    Arrays.avg(array) should equal(avgExpected)
+    Arrays.min(array) should equal(minExpected)
+    Arrays.max(array) should equal(maxExpected)
   }
-
 
 end ArraysTests
